97c5b6cf3bf4a03cb72f488a87305ce1dbf80dfc
Alfiia Enikeeva <aenykeeva@4lapy.ru>
2023-09-13 22:57:43 +0400

    create branch dev, create Products

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,38 @@
 import React from "react";
 import cx from "../../styles/Sidebar.module.css";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
+    const { list } = useSelector(({ categories }) => categories);
+
     return (
         <section className={cx.sidebar}>
             <div className={cx.title}>CATEGORIES</div>
             <nav>
                 <ui className={cx.menu}>
-                    <li>
-                        <NavLink>
-                            Link
-                        </NavLink>
-                    </li>
+                    {list.map(({ id, name }) => (
+                         <li key={id}>
+                         <NavLink 
+                            className={({ isActive }) => `${cx.link} ${isActive ? cx.active : ""}`}
+                            to={`/categories/${id}`}>{name}</NavLink>
+                     </li>
+                    ))}
                 </ui>
             </nav>
             <div className={cx.footer}>
-                <a href="/help" target="_blank" className={cx.link}>Help</a>
-                <a href="/terms" target="_blank" className={cx.link} style={{ textDecoration: "underline" }}>Terms</a>
+                <a 
+                    href="/help" 
+                    target="_blank" 
+                    className={cx.link}>
+                        Help
+                </a>
+                <a 
+                    href="/terms" 
+                    target="_blank" 
+                    className={cx.link} style={{ textDecoration: "underline" }}>
+                        Terms
+                </a>
             </div>
         </section>
     )
